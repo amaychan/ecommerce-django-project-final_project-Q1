a5afef781850bc0e08d63d045ee525eccdf65a9a
adehikmayati <adehikmayati@gmail.com>
2023-06-07 23:43:59 +0700

    add fitur filter satu product list

--- ecomm/toko/views.py
+++ ecomm/toko/views.py
@@ -15,6 +15,36 @@ class HomeListView(generic.ListView):
     template_name = 'home.html'
     queryset = ProdukItem.objects.all()
     paginate_by = 4
+    def get_context_data(self, *, object_list=None, **kwargs):
+        """Get the context for this view."""
+        filterCat = self.request.GET.get('category')
+        queryset = object_list if object_list is not None else self.object_list
+        if filterCat:
+            queryset= ProdukItem.objects.filter(kategori=filterCat)
+        page_size = self.get_paginate_by(queryset)
+
+        context_object_name = self.get_context_object_name(queryset)
+        if page_size:
+            paginator, page, queryset, is_paginated = self.paginate_queryset(
+                queryset, page_size
+            )
+            context = {
+                "paginator": paginator,
+                "page_obj": page,
+                "is_paginated": is_paginated,
+                "object_list": queryset,
+            }
+        else:
+            context = {
+                "paginator": None,
+                "page_obj": None,
+                "is_paginated": False,
+                "object_list": queryset,
+            }
+        if context_object_name is not None:
+            context[context_object_name] = queryset
+        context.update(kwargs)
+        return super().get_context_data(**context)
 
 class ProductDetailView(generic.DetailView):
     template_name = 'product_detail.html'
